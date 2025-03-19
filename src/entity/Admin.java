0e3577ef2598d36023b74b7e7072bf8dc802abd3
boyutov <minatomn654432@gmail.com>
2025-03-19 17:49:46 +0600

    Напишите Register

--- src/entity/Admin.java
+++ src/entity/Admin.java
@@ -4,7 +4,7 @@ import enums.UserType;
 
 //Класс который наследуется от User
 public class Admin extends User{
-    //Конструктор чтоб инициализировать переменную userType на Salesman
+    //Конструктор чтоб инициализировать переменную userType на Admin
     public Admin() {
         this.userType = UserType.ADMIN;
     }
